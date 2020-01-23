df4c6e2a6a0d72da8170ac9e50560bbd4cba8a17
Daniel Dopierała <dopierala.daniel@gmail.com>
2020-01-23 20:04:30 +0100

    Parametrize the algorithm with lower and upper bounds

--- PR/PR.cpp
+++ PR/PR.cpp
@@ -4,33 +4,61 @@
 #include <stdlib.h>
 #include <time.h>
 
-constexpr int NUMBER_COUNT = 1000;
+constexpr int MIN_NUMBER = 50;
+constexpr int MAX_NUMBER = 100;
+constexpr int NUMBER_COUNT = MAX_NUMBER - MIN_NUMBER + 1;
 
-int* initial_prime_numbers(int size)
+int* initial_prime_numbers()
 {
-	int* prime_numbers = (int*) malloc(size * sizeof(int));
+	int* prime_numbers = (int*) malloc(NUMBER_COUNT * sizeof(int));
 
-	for (int number = 0; number < size; number++)
+	for (int number = 0; number < NUMBER_COUNT; number++)
 	{
 		prime_numbers[number] = 1;
 	}
 
-	prime_numbers[0] = 0;
-	prime_numbers[1] = 0;
-
 	return prime_numbers;
 }
 
-void print_prime_numbers(int* prime_numbers, int size)
+int is_within_range(int number)
+{
+	return MIN_NUMBER <= number && number <= MAX_NUMBER;
+}
+
+int index_for_number(int number)
+{
+	if (is_within_range(number))
+	{
+		return number - MIN_NUMBER;
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+int number_for_index(int index)
+{
+	if (0 <= index && index < NUMBER_COUNT)
+	{
+		return index + MIN_NUMBER;
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+void print_prime_numbers(int* prime_numbers)
 {
 	printf("Prime numbers are: ");
 
-	for (int number = 0; number < size; number++)
+	for (int index = 0; index < NUMBER_COUNT; index++)
 	{
-		int is_prime = prime_numbers[number];
+		int is_prime = prime_numbers[index];
 		if (is_prime)
 		{
-			printf("%d ", number);
+			printf("%d ", number_for_index(index));
 		}
 	}
 
@@ -39,18 +67,28 @@ void print_prime_numbers(int* prime_numbers, int size)
 
 void calculate_prime_numbers()
 {
-	int* prime_numbers = initial_prime_numbers(NUMBER_COUNT + 1);
+	int* prime_numbers = initial_prime_numbers();
 
+	if (is_within_range(0)) {
+		prime_numbers[index_for_number(0)] = 0;
+	}
 
-	for (int number = 2; number <= NUMBER_COUNT; number++)
+	if (is_within_range(1)) {
+		prime_numbers[index_for_number(1)] = 0;
+	}
+
+	for (int number = 2; number <= MAX_NUMBER; number++)
 	{
-		for (int multiple = 2 * number; multiple <= NUMBER_COUNT; multiple = multiple + number)
+		for (int multiple = 2 * number; multiple <= MAX_NUMBER; multiple += number)
 		{
-			prime_numbers[multiple] = 0;
+			if (is_within_range(multiple))
+			{
+				prime_numbers[index_for_number(multiple)] = 0;
+			}
 		}
 	}
 
-	print_prime_numbers(prime_numbers, NUMBER_COUNT);
+	print_prime_numbers(prime_numbers);
 
 	delete prime_numbers;
 }
