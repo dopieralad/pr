35da0bb91baf4b823013cfa2a6ca3664eab08d52
Daniel Dopierała <dopierala.daniel@gmail.com>
2020-01-23 20:22:03 +0100

    Measure only the calculations

--- PR/PR.cpp
+++ PR/PR.cpp
@@ -6,7 +6,7 @@
 #include <time.h>
 
 constexpr int MIN_NUMBER = 0;
-constexpr int MAX_NUMBER = 100;
+constexpr int MAX_NUMBER = 1000000;
 constexpr int NUMBER_COUNT = MAX_NUMBER + 1;
 
 int* initial_prime_numbers()
@@ -37,7 +37,7 @@ void print_prime_numbers(int* prime_numbers)
 	printf("\n");
 }
 
-void calculate_prime_numbers()
+int* calculate_prime_numbers()
 {
 	int* prime_numbers = initial_prime_numbers();
 
@@ -56,16 +56,17 @@ void calculate_prime_numbers()
 		}
 	}
 
-	print_prime_numbers(prime_numbers);
-
-	delete prime_numbers;
+	return prime_numbers;
 }
 
-void measure(void (*procedure)())
+void measure_prime_calculations()
 {
 	clock_t start = clock();
-	procedure();
+	int* prime_numbers = calculate_prime_numbers();
 	clock_t stop = clock();
+
+	print_prime_numbers(prime_numbers);
+	delete prime_numbers;
 
 	double elapsed_seconds = ((stop - start) / 1000.0);
 	printf("Calculations took: '%lf' seconds.", elapsed_seconds);
@@ -73,5 +74,5 @@ void measure(void (*procedure)())
 
 int main()
 {
-	measure(calculate_prime_numbers);
+	measure_prime_calculations();
 }
