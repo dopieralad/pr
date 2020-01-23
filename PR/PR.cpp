cc80f054cec874c9d5a8220cbf38e21d6609c74c
Daniel Dopierała <dopierala.daniel@gmail.com>
2020-01-23 21:39:45 +0100

    Optimize memory usage

--- PR/PR.cpp
+++ PR/PR.cpp
@@ -6,23 +6,28 @@
 #include <time.h>
 
 typedef unsigned long long big_int;
+typedef char boolean;
+
 constexpr big_int MIN_NUMBER = 0;
-constexpr big_int MAX_NUMBER = 100;
+constexpr big_int MAX_NUMBER = 1000000000;
 constexpr big_int NUMBER_COUNT = MAX_NUMBER + 1;
 
-big_int* initial_prime_numbers()
+constexpr char TRUE = 1;
+constexpr char FALSE = 0;
+
+boolean* initial_prime_numbers()
 {
-	big_int* prime_numbers = (big_int*)malloc(NUMBER_COUNT * sizeof(big_int));
+	boolean* prime_numbers = (boolean*)malloc(NUMBER_COUNT * sizeof(boolean));
 
 	for (big_int number = 0; number < NUMBER_COUNT; number++)
 	{
-		prime_numbers[number] = 1;
+		prime_numbers[number] = TRUE;
 	}
 
 	return prime_numbers;
 }
 
-void print_prime_numbers(big_int* prime_numbers)
+void print_prime_numbers(boolean* prime_numbers)
 {
 	FILE* file;
 	fopen_s(&file, ".\\primes.txt", "w");
@@ -31,7 +36,7 @@ void print_prime_numbers(big_int* prime_numbers)
 	for (big_int number = MIN_NUMBER; number <= MAX_NUMBER; number++)
 	{
 		int is_prime = prime_numbers[number];
-		if (is_prime)
+		if (is_prime == TRUE)
 		{
 			found++;
 		}
@@ -60,21 +65,21 @@ void print_prime_numbers(big_int* prime_numbers)
 	fclose(file);
 }
 
-big_int* calculate_prime_numbers()
+boolean* calculate_prime_numbers()
 {
-	big_int* prime_numbers = initial_prime_numbers();
+	boolean* prime_numbers = initial_prime_numbers();
 
-	prime_numbers[0] = 0;
-	prime_numbers[1] = 0;
+	prime_numbers[0] = FALSE;
+	prime_numbers[1] = FALSE;
 
 	long double max_divisor = sqrt(MAX_NUMBER);
 	for (big_int divisor = 2; divisor <= max_divisor; divisor++)
 	{
-		if (prime_numbers[divisor] == 1) // Divisor is still considered prime
+		if (prime_numbers[divisor] == TRUE) // Divisor is still considered prime
 		{
 			for (big_int multiple = 2 * divisor; multiple <= MAX_NUMBER; multiple += divisor)
 			{
-				prime_numbers[multiple] = 0;
+				prime_numbers[multiple] = FALSE;
 			}
 		}
 	}
@@ -85,14 +90,14 @@ big_int* calculate_prime_numbers()
 void measure_prime_calculations()
 {
 	clock_t start = clock();
-	big_int* prime_numbers = calculate_prime_numbers();
+	boolean* prime_numbers = calculate_prime_numbers();
 	clock_t stop = clock();
-
-	print_prime_numbers(prime_numbers);
-	delete prime_numbers;
 
 	double elapsed_seconds = ((stop - start) / 1000.0);
 	printf("Calculations took: '%lf' seconds.", elapsed_seconds);
+
+	print_prime_numbers(prime_numbers);
+	delete prime_numbers;
 }
 
 int main()
