2e3959970f1dffd82c7b6d6ed54781a712d96f05
Daniel Dopierała <dopierala.daniel@gmail.com>
2020-01-25 12:29:02 +0100

    Parametrize the program

--- PR/PR.cpp
+++ PR/PR.cpp
@@ -10,22 +10,23 @@
 #include <math.h>
 #include <time.h>
 #include <omp.h>
+#include <windows.h>
 
 // Type aliases
 typedef signed long long big_int;
 
 // Configuration
 constexpr big_int OMP_THREADS = 8;
-constexpr big_int MIN_NUMBER = 0;
-constexpr big_int MAX_NUMBER = 1000000000;
+constexpr big_int MIN_NUMBER = 2;
+constexpr big_int MAX_NUMBER = 1100000000;
 constexpr big_int NUMBER_COUNT = MAX_NUMBER + 1;
 
-bool* initial_prime_numbers()
+bool* initial_prime_numbers(big_int number_count)
 {
-	bool* prime_numbers = (bool*)malloc(NUMBER_COUNT * sizeof(bool)); // Allocate enough memory
+	bool* prime_numbers = (bool*)malloc(number_count * sizeof(bool)); // Allocate enough memory
 
 	// Mark all numbers as primes initially
-	for (big_int number = 0; number < NUMBER_COUNT; number++)
+	for (big_int number = 0; number < number_count; number++)
 	{
 		prime_numbers[number] = true;
 	}
@@ -33,16 +34,16 @@ bool* initial_prime_numbers()
 	return prime_numbers;
 }
 
-void calculate_prime_numbers(bool* prime_numbers)
+void calculate_prime_numbers(bool* prime_numbers, big_int max_number)
 {
 	// Mark 0 and 1 as non-primes (by definition)
 	prime_numbers[0] = false;
 	prime_numbers[1] = false;
 
 	// Calculate maximal divisor that may exclude non-primes
-	big_int max_divisor = (big_int)ceil(sqrt(MAX_NUMBER));
+	big_int max_divisor = (big_int)ceil(sqrt(max_number));
 
-		#pragma omp parallel shared(prime_numbers, max_divisor)
+	#pragma omp parallel shared(prime_numbers, max_divisor)
 	{
 		printf("Thread: '%d' has started!\n", omp_get_thread_num());
 
@@ -54,7 +55,7 @@ void calculate_prime_numbers(bool* prime_numbers)
 			if (prime_numbers[divisor] == true)
 			{
 				// Mark all multiples as non-primes
-				for (big_int multiple = 2 * divisor; multiple <= MAX_NUMBER; multiple += divisor)
+				for (big_int multiple = 2 * divisor; multiple <= max_number; multiple += divisor)
 				{
 					prime_numbers[multiple] = false;
 				}
@@ -65,15 +66,15 @@ void calculate_prime_numbers(bool* prime_numbers)
 	}
 }
 
-void dump_prime_numbers(bool* prime_numbers)
+void dump_prime_numbers(bool* prime_numbers, big_int min_number, big_int max_number, double elapsed_seconds, int with_primes)
 {
 	// Open an output file
 	FILE* file;
-	fopen_s(&file, "C:\\Users\\Daniel\\Desktop\\primes.txt", "w");
+	fopen_s(&file, "C:\\Users\\Daniel\\Desktop\\primes.txt", "a");
 
 	// Count prime numbers
 	big_int found = 0;
-	for (big_int number = MIN_NUMBER; number <= MAX_NUMBER; number++)
+	for (big_int number = min_number; number <= max_number; number++)
 	{
 		bool is_prime = prime_numbers[number];
 		if (is_prime == true)
@@ -83,52 +84,69 @@ void dump_prime_numbers(bool* prime_numbers)
 	}
 
 	// Print summary line
-	fprintf_s(file, "Min: '%lli' Max: '%lli' Found: '%lli'\n", MIN_NUMBER, MAX_NUMBER, found);
+	fprintf_s(file, "Min: '%lli' Max: '%lli' Found: '%lli' Elapsed: '%lf'\n", min_number, max_number, found, elapsed_seconds);
 
-	// Print primes, 10 per each line
-	int line_counter = 1;
-	for (big_int number = MIN_NUMBER; number <= MAX_NUMBER; number++)
+	if (with_primes)
 	{
-		int is_prime = prime_numbers[number];
-		if (is_prime)
+		// Print primes, 10 per each line
+		int line_counter = 1;
+		for (big_int number = min_number; number <= max_number; number++)
 		{
-			fprintf_s(file, "%lli ", number);
-
-			if (line_counter++ >= 10)
+			int is_prime = prime_numbers[number];
+			if (is_prime)
 			{
-				line_counter = 1;
-				fprintf_s(file, "\n");
+				fprintf_s(file, "%lli ", number);
+
+				if (line_counter++ >= 10)
+				{
+					line_counter = 1;
+					fprintf_s(file, "\n");
+				}
 			}
 		}
-	}
 
-	// End a file with a newline
-	fprintf_s(file, "\n");
+		// End a file with a newline
+		fprintf_s(file, "\n");
+	}
 
 	// Close the output file
 	fclose(file);
 }
 
-int main()
+void process(int omp_threads, big_int min_number, big_int max_number, big_int number_count)
 {
 	// Set number of OpenMP threads
-	omp_set_num_threads(OMP_THREADS);
+	omp_set_num_threads(omp_threads);
 
 	// Get initial prime number array
-	bool* prime_numbers = initial_prime_numbers();
+	bool* prime_numbers = initial_prime_numbers(number_count);
 
 	// Measure prime number calculation time
 	clock_t start = clock();
-	calculate_prime_numbers(prime_numbers);
+	calculate_prime_numbers(prime_numbers, max_number);
 	clock_t stop = clock();
 
 	// Print calculation time to the console
 	double elapsed_seconds = ((stop - start) / 1000.0);
-	printf("Calculations took: '%lf' seconds.", elapsed_seconds);
+	printf("Calculations took: '%lf' seconds.\n", elapsed_seconds);
 
 	// Dump prime numbers to the output file
-	dump_prime_numbers(prime_numbers);
+	dump_prime_numbers(prime_numbers, min_number, max_number, elapsed_seconds, 0);
 
 	// Cleanup
 	delete prime_numbers;
+
+	Sleep(1000);
+}
+
+int main()
+{
+	for (int omp_threads = 1; omp_threads <= 4; omp_threads++)
+	{
+		process(omp_threads, MIN_NUMBER, MAX_NUMBER, NUMBER_COUNT);
+		process(omp_threads, MIN_NUMBER, MAX_NUMBER / 2, NUMBER_COUNT);
+		process(omp_threads, MAX_NUMBER / 2, MAX_NUMBER, NUMBER_COUNT);
+		process(omp_threads, MAX_NUMBER * 3 / 4, MAX_NUMBER, NUMBER_COUNT);
+		Sleep(5000);
+	}
 }
